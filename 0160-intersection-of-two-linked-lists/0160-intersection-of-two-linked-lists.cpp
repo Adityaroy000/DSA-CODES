02a4167d6d32b3f61cd4f957c4b0de10c2fbc8a6
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-10-01 21:05:25 +0530

    Time: 37 ms (59.6%), Space: 17.4 MB (24.67%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,20 +8,56 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA==NULL || headB == NULL) return NULL;
-        unordered_set<ListNode*>st;
-        ListNode* temp = headA;
+    int length(ListNode* head){
+        ListNode* temp = head;
+        int cnt =0;
         while(temp!=NULL){
-            st.insert(temp);
+            cnt++;
             temp = temp->next;
         }
-        while(headB!=NULL){
-            if(st.find(headB)!=st.end()){
-                return headB;
-            }
-            headB=headB->next;
+        return cnt;
+    }
+    int getdiff(ListNode* head1,ListNode* head2){
+        int a = length(head1);
+        int b = length(head2);
+        int diff;
+        if(a>b){
+            diff = a-b;
+        }else{
+            diff = b-a;
         }
+        return diff;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==NULL || headB == NULL) return NULL;
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+        int l1 = length(headA);
+        int l2 = length(headB);
+        int n = getdiff(headA,headB);
+        if(l1>l2){
+            ListNode*t = headA;
+            for(int i = 1;i<=n;i++){
+                t = t->next;
+            }
+            headA = t;
+        }else{
+            ListNode* t = headB;
+            for(int i=1;i<=n;i++){
+                t = t->next;
+            }
+            headB = t;
+        }
+        while(headA!=NULL&&headB!= NULL){
+            if(headA==headB){
+                return headA;
+            }else{
+                headA = headA->next;
+                headB = headB->next;
+            }
+        }
+        headA = t1;
+        headB = t2;
         return NULL;
     }
 };
