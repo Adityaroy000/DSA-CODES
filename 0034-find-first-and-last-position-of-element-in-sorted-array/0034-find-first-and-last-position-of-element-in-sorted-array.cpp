6cbca6fcc793ed1a94f965deb28831024ec11012
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-10-12 14:45:51 +0530

    Time: 7 ms (47.98%), Space: 16.4 MB (7.06%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -12,11 +12,8 @@ public:
                 first = mid;
                 high = mid-1;
             }
-            else if(nums[mid]<target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
+            else if(nums[mid]<target) low = mid+1;
+            else high = mid-1;
         }
         low = 0;
         high = n-1;
@@ -26,11 +23,8 @@ public:
                 last = mid;
                 low = mid+1;
             }
-            else if(nums[mid]<target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
+            else if(nums[mid]<target) low = mid+1;
+            else high = mid-1;
         }
         
         return {first,last};
