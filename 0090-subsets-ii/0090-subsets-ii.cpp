05436857b8d0bc0f1fa988b22eca462054c26451
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-10-25 07:16:12 +0530

    Time: 0 ms (100%), Space: 9 MB (59.33%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void find(vector<int>& nums,int n,vector<int>& temp,vector<vector<int>>& ans,int i,set<vector<int>>& st){
-        if(i==n){
-            if(st.find(temp)!=st.end()) return;
-            ans.push_back(temp);
-            st.insert(temp);
-            return;
+    void find(vector<int>& nums,int n,vector<int>& temp,vector<vector<int>>& ans,int i){
+        ans.push_back(temp);
+        
+        for(int j = i; j<n; j++){
+            if(j>i&&nums[j]==nums[j-1]) continue;
+            temp.push_back(nums[j]);
+            find(nums,n,temp,ans,j+1);
+            temp.pop_back();
         }
-        temp.push_back(nums[i]);
-        find(nums,n,temp,ans,i+1,st);
-        temp.pop_back();
-        find(nums,n,temp,ans,i+1,st);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
         vector<int>temp;
-        set<vector<int>>st;
         vector<vector<int>>ans;
-        find(nums,n,temp,ans,0,st);
+        find(nums,n,temp,ans,0);
         return ans;
     }
 };
