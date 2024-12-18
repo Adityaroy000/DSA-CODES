7a1e098e547c77fcdb9bc25507b715bde63ed6b5
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-12-18 09:07:04 +0530

    Time: 0 ms (100%), Space: 55.7 MB (21.19%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,22 +1,22 @@
 class Twitter {
 public:
-    int timestamp;
-    unordered_map<int,vector<pair<int,int>>>tweets;
-    unordered_map<int,unordered_set<int>>follower;
-    Twitter():timestamp(0) {}
+    int timestamp;//global timestamp to keep track of order of tweets
+    unordered_map<int,vector<pair<int,int>>>tweets;//userId,{timestamp,tweetId}
+    unordered_map<int,unordered_set<int>>follower;//userId,set of followers
+    Twitter():timestamp(0) {} // for each object twitter timestamp will be created
     
     void postTweet(int userId, int tweetId) {
         tweets[userId].push_back({timestamp++,tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
-        priority_queue<pair<int,int>>feed;
-        if(tweets.find(userId)!=tweets.end()){
+        priority_queue<pair<int,int>>feed;//most recent tweets will be at top wrt to timestamp
+        if(tweets.find(userId)!=tweets.end()){//Include the user's own tweets
             for(const auto& tweet : tweets[userId]){
                 feed.push(tweet);
             }
         }
-        if(follower.find(userId)!=follower.end()){
+        if(follower.find(userId)!=follower.end()){// Include tweets from users the current user follows
             for(int followeeId : follower[userId]){
                 if(tweets.find(followeeId)!=tweets.end()){
                     for(const auto& tweet : tweets[followeeId]){
@@ -25,6 +25,7 @@ public:
                 }
             }
         }
+        // Extract the top 10 most recent tweets
         vector<int>ans;
         int cnt = 0;
         while(!feed.empty() && cnt < 10){
@@ -36,7 +37,7 @@ public:
     }
     
     void follow(int followerId, int followeeId) {
-        if(followerId!=followeeId){
+        if(followerId!=followeeId){// Prevent self-following
             follower[followerId].insert(followeeId);
         }
     }
