3af7a786fe2c6deaa5c0ea415981b053d8be8c09
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-12-25 18:46:02 +0530

    Time: 7 ms (81.46%), Space: 38.9 MB (18.2%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    void dfs(int row,int col,vector<vector<int>>& vis,vector<vector<int>>& grid,int drow[],int dcol[],int& cnt){
+        vis[row][col]=1;
+        cnt++;
+        for(int i = 0; i<4; i++){
+            int nrow = row+drow[i];
+            int ncol = col+dcol[i];
+            if(nrow<grid.size() && nrow>=0 && ncol<grid[0].size() && ncol>=0 && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                dfs(nrow,ncol,vis,grid,drow,dcol,cnt);
+            }
+        }
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        vector<vector<int>>vis(r,vector<int>(c,0));
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+        int cnt = 0;
+        int count = 0;
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++){
+                if(grid[i][j]==1) count++;
+            }
+        }
+        for(int j = 0; j<c; j++){
+            if(grid[0][j]==1 && vis[0][j]==0){
+                dfs(0,j,vis,grid,drow,dcol,cnt);
+            }
+            if(grid[r-1][j]==1 && vis[r-1][j]==0){
+                dfs(r-1,j,vis,grid,drow,dcol,cnt);
+            }
+        }
+        for(int i = 0; i<r; i++){
+            if(grid[i][0]==1 && vis[i][0]==0){
+                dfs(i,0,vis,grid,drow,dcol,cnt);
+            }
+            if(grid[i][c-1]==1 && vis[i][c-1]==0){
+                dfs(i,c-1,vis,grid,drow,dcol,cnt);
+            }
+        }
+        return (count-cnt);
+    }
+};
