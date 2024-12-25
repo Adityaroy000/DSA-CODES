c2c11785d0c66221e0c652ba68c5e21600fdad7f
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-12-25 19:32:59 +0530

    Time: 9 ms (58.11%), Space: 32.1 MB (63.15%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,14 +1,12 @@
 class Solution {
 private:
-    void bfs(int row,int col,vector<vector<int>>& vis,vector<vector<int>>& grid,int drow[],int dcol[],int& cnt){
-        queue<pair<int,int>>q;
-        q.push({row,col});
-        vis[row][col]=1;
+    void bfs(vector<vector<int>>& vis,vector<vector<int>>& grid,queue<pair<int,int>>q){
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.empty()){
             int rid = q.front().first;
             int cid = q.front().second;
             q.pop();
-            cnt++;
             for(int i = 0; i<4; i++){
                 int nrow = rid+drow[i];
                 int ncol = cid+dcol[i];
@@ -24,31 +22,34 @@ public:
         int r = grid.size();
         int c = grid[0].size();
         vector<vector<int>>vis(r,vector<int>(c,0));
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
-        int cnt = 0;
         int count = 0;
-        for(int i = 0; i<r; i++){
-            for(int j = 0; j<c; j++){
-                if(grid[i][j]==1) count++;
-            }
-        }
+        queue<pair<int,int>>q;
         for(int j = 0; j<c; j++){
             if(grid[0][j]==1 && vis[0][j]==0){
-                bfs(0,j,vis,grid,drow,dcol,cnt);
+                q.push({0,j});
+                vis[0][j]=1;
             }
             if(grid[r-1][j]==1 && vis[r-1][j]==0){
-                bfs(r-1,j,vis,grid,drow,dcol,cnt);
+                q.push({r-1,j});
+                vis[r-1][j]=1;
             }
         }
         for(int i = 0; i<r; i++){
             if(grid[i][0]==1 && vis[i][0]==0){
-                bfs(i,0,vis,grid,drow,dcol,cnt);
+                q.push({i,0});
+                vis[i][0]=1;
             }
             if(grid[i][c-1]==1 && vis[i][c-1]==0){
-                bfs(i,c-1,vis,grid,drow,dcol,cnt);
+                q.push({i,c-1});
+                vis[i][c-1]=1;
             }
         }
-        return (count-cnt);
+        bfs(vis,grid,q);
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++){
+                if(grid[i][j]==1 && !vis[i][j]) count++;
+            }
+        }
+        return count;
     }
 };
