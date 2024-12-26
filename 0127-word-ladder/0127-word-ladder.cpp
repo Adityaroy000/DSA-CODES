bb98ff41f5c1ecace5aae9d5b6b7a2646e89d005
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2024-12-26 17:36:28 +0530

    Time: 52 ms (86.58%), Space: 21.2 MB (41.61%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -3,35 +3,31 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         int n = wordList.size();
         int s = wordList[0].size();
-        int cnt1=0;
-        for(int i = 0 ;i<n; i++){
-            if(endWord == wordList[i]) cnt1++;
+        unordered_set<string>st;
+        for(int i = 0; i<n; i++){
+            st.insert(wordList[i]);
         }
-        if(cnt1==0) return 0;
-        queue<string>q;
-        q.push(beginWord);
-        vector<int>vis(n,0);
-        int count = 1;
+        if(st.find(endWord)==st.end()){
+            return 0;
+        }
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
         while(!q.empty()){
-            int size = q.size();
-            for(int k=0;k<size;k++){
-                auto it = q.front();
-                q.pop();
-                for(int i = 0; i<n; i++){
-                    int cnt  = 0;
-                    if(vis[i]==0){
-                        for(int j=0;j<s;j++){
-                            if(it[j]!=wordList[i][j]) cnt++;
-                        }
-                        if(cnt == 1){
-                            q.push(wordList[i]);
-                            vis[i]=1;
-                            if(wordList[i]==endWord) return count+1;
-                        }
+            string str = q.front().first;
+            int step = q.front().second;
+            q.pop();
+            for(int i=0;i<str.length();i++){
+                char oc = str[i];
+                for(char ch = 'a'; ch<='z'; ch++){
+                    str[i]=ch;
+                    if(st.find(str)!=st.end()){
+                        if(str==endWord) return step+1;
+                        q.push({str,step+1});
+                        st.erase(str);
                     }
                 }
+                str[i]=oc;
             }
-            count++;
         }
         return 0;
     }
