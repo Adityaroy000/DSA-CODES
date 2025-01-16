54777648c56b5fe8a791a02627c44c087c0b5c2f
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-01-16 14:20:50 +0530

    Time: 0 ms (100%), Space: 15.6 MB (76.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -13,7 +13,7 @@ public:
             reverse(nums.begin(),nums.end());
             return;
         }
-        for(int i = n-1; i>0; i--){
+        for(int i = n-1; i>=0; i--){
             if(nums[i]>nums[index]){
                 swap(nums[i],nums[index]);
                 break;
