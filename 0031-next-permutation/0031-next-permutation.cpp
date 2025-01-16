faac828d8946557d6b4af7cebe4b56a078d530c7
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-01-16 14:20:27 +0530

    Time: 0 ms (100%), Space: 15.6 MB (55.94%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-vector<int>permut(vector<int>& nums){
-    int n = nums.size();
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
         int index = -1;
-        for(int i =n-2;i>=0;i--){
+        for(int i = n-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
                 index = i;
                 break;
@@ -11,21 +11,14 @@ vector<int>permut(vector<int>& nums){
         }
         if(index == -1){
             reverse(nums.begin(),nums.end());
-            return nums;
+            return;
         }
-        for(int i =n-1; i>index;i--){
+        for(int i = n-1; i>0; i--){
             if(nums[i]>nums[index]){
                 swap(nums[i],nums[index]);
                 break;
             }
         }
-    reverse(nums.begin()+index+1,nums.end());
-    return nums;
-}
-    void nextPermutation(vector<int>& nums) {
-        vector<int>ans = permut(nums);
-        for(auto it : ans){
-            cout<<it<<" ";
-        }
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
