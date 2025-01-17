e9e2ccab9217b3b77cdb4b5a4935a5fdadb90453
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-01-17 09:26:34 +0530

    Time: 0 ms (100%), Space: 19.1 MB (5.8%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-         int col0 = 1;
-    //Traverse the matrix and
-    // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                matrix[i][0] = 0;
-
-                if (j != 0)
-                    matrix[0][j] = 0;
-                else
-                    col0 = 0;
-            }
-        }
-    }
-
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
+        int r = matrix.size();
+        int c = matrix[0].size();
+        int r0=-1,c0=-1;
+        vector<pair<int,int>> id;
+        vector<vector<int>> vis(r, vector<int>(c, 0));
+        for(int i=0; i<r; i++){
+            for(int j = 0; j<c; j++){
+                if(matrix[i][j]==0){
+                    id.push_back({i,j});
+                    r0 = i;
+                    c0 = j;
+                    vis[r0][c0] = 1;
                 }
             }
         }
-    }
-
-    if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
+        if(r0 == -1 && c0 == -1) return;
+        for(auto it : id){
+            int rowid = it.first;
+            int colid = it.second;
+            for(int j = 0; j<c; j++){
+                if(vis[rowid][j]!=1){
+                    matrix[rowid][j] = 0;
+                    vis[rowid][j] = 1;
+                }
+            }
+            for(int i = 0; i<r; i++){
+                if(vis[i][colid]!=1){
+                    matrix[i][colid] = 0;
+                    vis[i][colid] = 1;
+                }
+            }
         }
-    }
-    if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 0;
-        }
-    }
     }
 };
