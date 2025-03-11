800e401d47cf2c8ee9cfd66da06696cad2516f90
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-11 12:32:11 +0530

    Time: 0 ms (100%), Space: 59.4 MB (5.11%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -6,8 +6,10 @@ public:
         int maxlen  = 0,len = 0,cntz = 0;
         while(r<n){
             if(nums[r]==0) cntz++;
-            while(cntz>k){
-                if(nums[l]==0) cntz--;
+            if(cntz>k){
+                if(nums[l]==0) {
+                    cntz--;
+                }
                 l++;
             } 
             if(cntz<=k){
