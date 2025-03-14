d42e63ae5e0c237eae417d34d3848313f8f2bc2e
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-14 23:37:44 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.98%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+//here, single element will always present at even idx so, there is a pattern : in left search space of single element, the equal ele are in the form (even,odd) indexs and to right of single ele , ele are in (odd,even) indexs.
+//(mid%2==0)&&nums[mid]==nums[mid+1] -> this condition means we have a ele at even, if we get same ele ahead of nums[mid] means at odd index then we will have (even,odd) that means we are left of single element... so low = mid+1
+//(mid%2!=0)&&nums[mid]==nums[mid-1] -> this means we have a ele at odd index and if we get a same ele at left of it (that means at even index)then we will have (even,odd) then again we are in left of single element... so low = mid+1;
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
