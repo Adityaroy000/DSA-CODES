480b41059fef26854d3d777b1038177fd0ad1bfc
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-14 16:00:15 +0530

    Time: 3 ms (80.73%), Space: 11.6 MB (41.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -11,27 +11,17 @@ public:
         int sidx = -1;
         int l=0,r=0;
         int cnt  = 0;
-        unordered_map<char,int>mp;
-        while(r<m){
-            
-            if(mpp.find(s[r])!=mpp.end()){
-                mp[s[r]]++;
-                if(mp[s[r]]==mpp[s[r]]){
-                    cnt++;
-                }
-            }
         
-            while(cnt == mpp.size()){
+        while(r<m){
+            if(mpp[s[r]]>0) cnt++;
+            mpp[s[r]]--;
+            while(cnt == n){
                 if(r-l+1 < mini){
                     mini = r-l+1;
                     sidx = l;
                 }
-                if(mpp.find(s[l])!=mpp.end()){
-                    if(mp[s[l]]==mpp[s[l]]) cnt--;
-                    mp[s[l]]--;
-                    if(mp[s[l]]==0) mp.erase(s[l]);
-                }
-                
+                mpp[s[l]]++;
+                if(mpp[s[l]]>0) cnt--;
                 l++;
             }
             r++;
