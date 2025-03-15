d3973d48cb8c8b784e4188b97db662d03ecbe9cc
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-15 15:33:18 +0530

    Time: 4 ms (89%), Space: 26.1 MB (81.8%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -4,19 +4,20 @@ public:
         int n = nums.size();
         int low = 1;
         int high = *max_element(nums.begin(),nums.end());
+
         int ans = -1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            int cnt = 0;
-            for(int i = 0; i<n; i++){
-                if(nums[i]%mid == 0) cnt += (nums[i]/mid);
-                else cnt += (nums[i]/mid)+1;
+            int sum = 0;
+            for(int i = 0;i<n;i++){
+                sum += (nums[i]+mid-1)/mid;
+                if(sum>threshold) break;
             }
-            if(cnt <= threshold){
+            if(sum <= threshold) {
                 ans = mid;
                 high = mid-1;
-            }
-            else low = mid+1;
+            }else low = mid+1;
+
         }
         return ans;
     }
