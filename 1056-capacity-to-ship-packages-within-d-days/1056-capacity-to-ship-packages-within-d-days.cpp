e5a69a83bc358179ba1e5808e0719f2f21509e9a
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-16 09:13:05 +0530

    Time: 9 ms (79.89%), Space: 34.9 MB (65.71%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
+        int maxc = 0;
+        for(int i = 0;i<n; i++){
+            maxc += weights[i];
+        }
+        int high = maxc;
+        int low = *max_element(weights.begin(),weights.end());
         int ans = -1;
-        int mini = *max_element(weights.begin(),weights.end());
-        int maxi = accumulate(weights.begin(),weights.end(),0);
-        int low = mini,high = maxi;
         while(low<=high){
             int mid = low+(high-low)/2;
-            int cnt = 0,day = 0;
-            for(int k = 0;k<n; k++){
-                cnt += weights[k];
-                if(cnt>mid){
-                    day++;
-                    cnt = weights[k];
-                    if(cnt>mid) break;
+            int sum = 0,cnt = 0;
+            int i = 0;
+            while(i<n){
+                if(sum+weights[i] > mid) {
+                    cnt++;
+                    sum = 0;
+                }else {
+                    sum+=weights[i];
+                    i++;
                 }
+                if(cnt>days) break;
             }
-            if(cnt <= mid) day++;
-            if(day <= days){
+            
+            if(cnt+1 <= days) {
                 ans = mid;
                 high = mid-1;
-            }else low = mid+1; 
+            }else low = mid+1;
         }
         return ans;
     }
