a61af24c49d8cbc5c4da552e4d7528c4d5535205
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-23 21:59:51 +0530

    Time: 0 ms (100%), Space: 12.7 MB (72.89%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,14 +3,12 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m-1].size();
-        vector<int>lastrow(m);
+        vector<int>lastrow(m,0),temp(m,0);
         for(int i = 0;i<n;i++){
             lastrow[i] = triangle[m-1][i];
         }
         for(int i = m-2;i>=0; i--){
-            int s = triangle[i].size();
-            vector<int>temp(s);
-            for(int j = s-1;j>=0; j--){
+            for(int j = i;j>=0; j--){
                 int up = triangle[i][j] + lastrow[j];
                 int diag = triangle[i][j] + lastrow[j+1];
                 temp[j]= min(up,diag);
