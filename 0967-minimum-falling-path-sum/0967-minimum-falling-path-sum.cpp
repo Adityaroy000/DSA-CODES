a790bc3bfc3f5fe298f27be7f7bef652cdd6c7f1
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-24 09:40:48 +0530

    Time: 0 ms (100%), Space: 14.4 MB (90.61%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -3,13 +3,12 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int ans = 1e9;
-        vector<int>prevrow(n);
+        vector<int>prevrow(n),temp(n);
         for(int i =0;i<n; i++){
             prevrow[i] = matrix[0][i];
         }
         
         for(int i = 1;i<n; i++){
-            vector<int>temp(n);
             for(int j = 0;j<n; j++){
                 int up = matrix[i][j] + prevrow[j];
                 int rdiag = 1e9,ldiag = 1e9;
