cbcfddb763ec148c31f2fb9ee6cc023288e98b68
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-03-27 11:13:55 +0530

    Time: 107 ms (66.91%), Space: 16.8 MB (75.9%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -5,25 +5,24 @@ public:
         int sum = accumulate(arr.begin(),arr.end(),0);
         if(sum%2 != 0) return false;
         int ts = sum/2;
-        vector<vector<bool>>dp(n,vector<bool>(ts+1,false));
-        for(int i=0;i<n;i++){
-            dp[i][0] = true;
-        }
-        
-        if(arr[0]<=ts){
-            dp[0][arr[0]] = true;
-        }
+        vector<bool>prev(sum+1,false);
+        prev[0] = true;
+        prev[arr[0]] = true;
         //Fill dp table
         for(int i=1;i<n;i++){
+            vector<bool>curr(ts+1,false);
+            curr[0] = true;
             for(int j=1;j<=ts;j++){//j->target
-                bool nottake = dp[i-1][j];
+                bool nottake = prev[j];
                 bool take = false;
                 if(arr[i]<=j){
-                    take = dp[i-1][j-arr[i]];
+                    take = prev[j-arr[i]];
                 }
-                dp[i][j] = take||nottake;
+                curr[j] = take||nottake;
             }
+            prev = curr;
         }
-        return dp[n-1][ts];
+        
+        return prev[ts];
     }
 };
