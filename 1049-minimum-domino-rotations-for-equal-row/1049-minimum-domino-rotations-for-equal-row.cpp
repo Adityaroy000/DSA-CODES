08e315530b7b1a5aafcd511d256a359d875862b9
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-05-03 16:09:02 +0530

    Time: 3 ms (67.46%), Space: 115.4 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,88 +1,24 @@
 class Solution {
 public:
+    int find(vector<int>& tops, vector<int>& bottoms,int val){
+        int n = tops.size();
+        int swaptop = 0;
+        int swapbot = 0;
+        for(int i = 0;i<n;i++){
+            if(tops[i]!=val&&bottoms[i]!=val) return -1;
+            else if(tops[i]!=val) swaptop++;
+            else if(bottoms[i]!=val) swapbot++;
+        }
+        return min(swapbot,swaptop);
+    }
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int t = tops.size();
-        int b = bottoms.size();
-
-        unordered_map<int,int>mpptop;
-        unordered_map<int,int>mppbot;
-        for(int i = 0;i<t;i++){
-            mpptop[tops[i]]++;
+        int n = tops.size();
+        int ans = INT_MAX;
+        for(int i = 1;i<=6;i++){
+            int steps = find(tops,bottoms,i);
+            if(steps != -1) ans = min(ans,steps);
         }
-        for(int i = 0;i<b;i++){
-            mppbot[bottoms[i]]++;
-        }
-        int max_freq_top = -1,max_freq_bot =-1;
-        int cnt = 0;
-        if(mpptop.size()<mppbot.size()){
-            int check = 0;
-            for(auto &it:mpptop){
-                if(it.second>check) {
-                    check = it.second;
-                    max_freq_top = it.first;
-                }
-            }
-            for(int i = 0;i<t;i++){
-                if(tops[i]!=max_freq_top){
-                    if(bottoms[i]==max_freq_top){
-                        swap(tops[i],bottoms[i]);
-                        cnt++;
-                    }else return -1;
-                }
-            }
-        }else if(mpptop.size()>mppbot.size()){
-            int check = 0;
-            for(auto &it:mppbot){
-                if(it.second>check) {
-                    check = it.second;
-                    max_freq_bot = it.first;
-                }
-            }
-            for(int i = 0;i<b;i++){
-                if(bottoms[i]!=max_freq_bot){
-                    if(tops[i]==max_freq_bot){
-                        swap(tops[i],bottoms[i]);
-                        cnt++;
-                    }else return -1;
-                }
-            }
-        }else{
-            int check = 0;
-            for(auto &it:mpptop){
-                if(it.second>check) {
-                    check = it.second;
-                    max_freq_top = it.first;
-                }
-            }
-            int check1 = 0;
-            for(auto &it:mppbot){
-                if(it.second>check1) {
-                    check1 = it.second;
-                    max_freq_bot = it.first;
-                }
-            }
-            int topelefreq = mpptop[max_freq_top];
-            int botelefreq = mppbot[max_freq_bot];
-            if(topelefreq<=botelefreq){
-                for(int i = 0;i<b;i++){
-                    if(bottoms[i]!=max_freq_bot){
-                        if(tops[i]==max_freq_bot){
-                            swap(tops[i],bottoms[i]);
-                            cnt++;
-                        }else return -1;
-                    }
-                }
-            }else{
-                for(int i = 0;i<t;i++){
-                    if(tops[i]!=max_freq_top){
-                        if(bottoms[i]==max_freq_top){
-                            swap(tops[i],bottoms[i]);
-                            cnt++;
-                        }else return -1;
-                    }
-                }
-            }
-        }
-        return cnt;
+        if(ans == INT_MAX) return -1;
+        else return ans;
     }
 };
