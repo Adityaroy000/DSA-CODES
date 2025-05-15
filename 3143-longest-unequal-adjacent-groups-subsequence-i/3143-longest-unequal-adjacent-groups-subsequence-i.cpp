500b652ea3d8391af7784e36a36e7afba5cd310b
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-05-15 12:32:54 +0530

    Time: 0 ms (100%), Space: 30 MB (40.56%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -4,24 +4,8 @@ public:
         int n = groups.size();
         vector<string>ans;
 
-        int maxlen = 0;
         for(int i=0;i<n;i++){
-            int cnt = 1;
-            int check = groups[i];
-            vector<string>temp;
-            temp.push_back(words[i]);
-            for(int j=i+1;j<n;j++){
-                if(check != groups[j]){
-                    cnt++;
-                    temp.push_back(words[j]);
-                    check = groups[j];
-
-                }
-            }
-            if(cnt>maxlen){
-                maxlen = cnt;
-                ans = temp;
-            }
+            if(i==0 || groups[i]!=groups[i-1]) ans.push_back(words[i]);
         }
         return ans;
     }
