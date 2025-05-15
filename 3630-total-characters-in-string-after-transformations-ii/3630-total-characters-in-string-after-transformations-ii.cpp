9f71727114e4667a311f8f7151fd91b9a46d1200
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-05-15 11:41:05 +0530

    Time: 383 ms (34.67%), Space: 53.5 MB (64%) - LeetHub

--- 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.cpp
+++ 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.cpp
@@ -1,29 +1,67 @@
+using ll = long long;
 class Solution {
 public:
+    typedef vector<vector<int>> matrix;
     int mod = 1e9+7;
-    int lengthAfterTransformations(string s, int t, vector<int>& nums) {
-        int n = s.length();
-        vector<int>mpp(26,0);
-        for(int i = 0;i<n;i++){
-            mpp[s[i]-'a']++;
-        }
 
-        for(int j = 1;j<=t;j++){
-            vector<int>temp(26,0);
-            for(int i = 0;i<26;i++){
-                char ch = i+'a';
-                int freq = mpp[i];
-                int size = nums[ch-'a'];
-                for(int k = 1;k<=size;k++){
-                    temp[(i+k)%26] = (temp[(i+k)%26]+freq)%mod;
+    matrix matrixMultiply(matrix& m1,matrix& m2){
+        matrix ans(26,vector<int>(26,0));
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                for(int k=0;k<26;k++){
+                    ans[i][j] = (ans[i][j]+(1LL*m1[i][k]*m2[k][j])%mod)%mod;
                 }
             }
-            mpp = move(temp);
         }
-        int res = 0;
-        for(int i=0;i<26;i++ ){
-            res = (res + mpp[i])%mod;
+        return ans;
+    }
+
+    matrix matrixExp(matrix& base, int exponent){
+        if(exponent == 0){
+            matrix I(26,vector<int>(26,0));
+            for(int i=0;i<26;i++){
+                I[i][i] = 1;
+            }
+            return I;
         }
+
+        matrix half = matrixExp(base,exponent/2);
+        matrix res = matrixMultiply(half,half);
+        if(exponent%2==1){
+            res = matrixMultiply(res,base);
+        }
+
         return res;
+    }
+
+    int lengthAfterTransformations(string s, int t, vector<int>& nums) {
+        vector<int>initialFreq(26,0);
+        for(char &ch:s){
+            initialFreq[ch-'a']++;
+        }
+        //T matrix
+        matrix T(26,vector<int>(26,0));
+        for(int i=0;i<26;i++){
+            for(int add = 1;add<=nums[i];add++){
+                T[(i+add)%26][i] ++;
+            }
+        }
+        
+        //T^T
+        matrix res = matrixExp(T,t);
+
+        vector<int>newFreq(26,0);
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                newFreq[i]= (newFreq[i]+(1LL*res[i][j]*initialFreq[j])%mod)%mod;
+            }
+        }
+
+        int totallen = 0;
+        for(int i=0;i<26;i++){
+            totallen = (totallen+newFreq[i])%mod; 
+        }
+        return totallen;
+
     }
 };
