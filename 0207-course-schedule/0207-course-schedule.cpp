a55ff7be982e1badbe9a4d8100d23e0276925a02
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-05-26 19:01:24 +0530

    Time: 2 ms (83.8%), Space: 19.1 MB (87.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        int n = prerequisites.size();
-        for(int i = 0; i<n; i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        int n = numCourses;
+        vector<int>indeg(n);
+        vector<vector<int>>adj(n);
+
+        for(auto &it:prerequisites){
+            int u = it[0];
+            int v = it[1];
+
+            indeg[u]++;
+            adj[v].push_back(u);
         }
-        int V = numCourses;
-        vector<int>indeg(V,0);
-        for(int i = 0; i<V; i++){
-            for(auto it : adj[i]){
-                indeg[it]++;
-            }
-        }
+
         queue<int>q;
-        for(int i = 0; i<V; i++){
+        for(int i=0;i<n;i++){
             if(indeg[i]==0) q.push(i);
         }
-        int count = 0;
+        if(q.empty()) return false;
+        int cnt = 0;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            count++;
-            for(auto it:adj[node]){
-                indeg[it]--;
-                if(indeg[it]==0) q.push(it);
+
+            cnt++;
+            for(auto& v : adj[node]){
+                indeg[v]--;
+                if(indeg[v]==0) q.push(v);
             }
         }
-        return count==V;
+
+        if(cnt!=n) return false;
+
+        return true;
     }
 };
