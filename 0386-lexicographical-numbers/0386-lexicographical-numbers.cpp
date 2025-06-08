6347987632797313f6a90bbdb3e65e0d131b7cfb
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-06-08 09:38:30 +0530

    Time: 0 ms (100%), Space: 14.1 MB (69.54%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int>ans;
+        int count = 1;
+        for(int i=0;i<n;i++){
+            ans.push_back(count);
+            if(count*10 <= n){
+                count = count*10;
+            }else{
+                if( count >= n) count /= 10;
+                count++;
+                while(count % 10 == 0) count /= 10;
+            }
+        }
+        return ans;
+    }
+};
