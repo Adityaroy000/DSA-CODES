e34fb182704e1b4375e022e290b76f4ecbf23e62
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-06-19 10:57:14 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.85%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -8,16 +8,24 @@ public:
         while(low<=high){
             int mid = low+(high-low)/2;
             int sum = 0,cnt = 0;
+            bool valid = true;
             for(int i=0;i<n;i++){
+                if(nums[i]>mid) {
+                    valid = false;
+                    break;
+                }
                 sum+=nums[i];
                 if(sum > mid){
                     cnt++;
                     sum = nums[i];
-                    if(cnt+1 > k) break;
+                    if(cnt+1 > k) {
+                        valid = false;
+                        break;
+                    }
                 }
             }
-            cnt++;
-            if(cnt<=k){
+            if(valid) cnt++;
+            if(valid && cnt<=k){
                 ans = mid;
                 high = mid-1;
             }else low = mid+1;
