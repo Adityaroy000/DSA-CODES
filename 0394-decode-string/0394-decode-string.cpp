71fada109aa7dfe32795464e130bc2e6a530a19b
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-06-23 08:07:38 +0530

    Time: 0 ms (100%), Space: 9.4 MB (53.25%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     string decodeString(string s) {
         int n = s.size();
+  stack<string>st;
+  stack<int>digit;
 
-        stack<string>st;
-        stack<int>digit;
-        string curr = "";
-        for(int i=0;i<n;i++){
-            if(isdigit(s[i])){
-                int num = 0;
-                while(i<s.size() && isdigit(s[i])){
-                    num = num*10 + (s[i]-'0');
-                    i++;
-                }
-                digit.push(num);
-                i--;
-            }else if(s[i] == '['){
-                st.push(curr);
-                curr = "";
-            }else if(s[i] == ']'){
-                string temp = "";
-                string prev = st.top();st.pop();
-                int dig = digit.top();digit.pop();
+  string curr = "";
+  for(int i=0;i<n;i++){
+    if(isdigit(s[i])){
+      int num = 0;
+      while(i<s.size() && isdigit(s[i])){
+        num = num*10 +(s[i]-'0');
+        i++;
+      }
+      i--;
+      digit.push(num);
+    }else if(s[i] == '['){
+      st.push(curr);
+      curr = "";
+    }else if(s[i]==']'){
+      string temp = "";
+      string prev = st.top(); st.pop();
+      int dig = digit.top(); digit.pop();
 
-                for(int j= 0;j<dig;j++){
-                    temp+=curr;
-                }
-                curr = prev + temp;
-            }else{
-                curr += s[i];
-            }
-        }
-        return curr;
+      for(int count=0;count<dig;count++){
+        temp += curr;
+      }
+      curr = prev+temp;
+    }else{
+      curr += s[i];
+    }
+  }
+  return curr;
     }
 };
