70a5f1950993417adc46da500ccc1e5c91f7e0c7
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-07-16 06:40:41 +0530

    Time: 3 ms (32.58%), Space: 16.3 MB (8.42%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> seq(vector<int>& nums,vector<int>& temp,int i){
-        vector<vector<int>>req;
-        if(i==nums.size()){
-            req.push_back(temp);
-            return req;
-        } 
-        temp.push_back(nums[i]);
-        vector<vector<int>>inc = seq(nums,temp,i+1);
-        temp.pop_back();
-        vector<vector<int>>exc = seq(nums,temp,i+1);
-        req.insert(req.end(),inc.begin(),inc.end());
-        req.insert(req.end(),exc.begin(),exc.end());
-        return req;
+    void solve(vector<int>& arr,vector<int> nums,vector<vector<int>>& ans){
+        if(nums.size()==0){
+            ans.push_back(arr);
+            return;
+        }
+
+        vector<int>a1 = arr;
+        vector<int>a2 = arr;
+        a2.push_back(nums[0]);
+
+        nums.erase(nums.begin()+0);
+
+        solve(a1,nums,ans);
+        solve(a2,nums,ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>temp;
-        return seq(nums,temp,0); 
+        vector<int>arr;
+
+        vector<vector<int>> ans;
+
+        solve(arr,nums,ans);
+        return ans;
     }
 };
