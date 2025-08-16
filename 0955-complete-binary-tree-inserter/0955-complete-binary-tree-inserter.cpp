dd48109aeed85b3049e0c850197e277d90bb1916
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-08-16 18:07:10 +0530

    Time: 0 ms (100%), Space: 27.3 MB (39.34%) - LeetHub

--- 0955-complete-binary-tree-inserter/0955-complete-binary-tree-inserter.cpp
+++ 0955-complete-binary-tree-inserter/0955-complete-binary-tree-inserter.cpp
@@ -12,109 +12,33 @@
 class CBTInserter {
 public:
     TreeNode* node;
+    queue<TreeNode*>q;
 
     CBTInserter(TreeNode* root) {
         node = root;
-    }
-    int depth = -1;
-    bool check(){
-        queue<TreeNode*>q;
-        q.push(node);
-        int level = 0;
-        while(!q.empty()){
-            int n = q.size();
-            int cnt = 0;
-            while(n--){
-                TreeNode* temp = q.front();
-                if(temp->left) q.push(temp->left);
-                if(temp->right) q.push(temp->right);
-                cnt++;
-                q.pop();
-            }
-            if(cnt != pow(2,level)) {
-                depth = level;
-                return false;
-            }
-            level++;
-        }
-        return true;
-    }
-    vector<TreeNode*>temp;
-    void solve(int odepth){
-        queue<TreeNode*>q;
-        q.push(node);
-        int lev = 0;
-        while(!q.empty()){
-            int n = q.size();
-            if(lev == odepth){
-                while(!q.empty()){
-                    temp.push_back(q.front());
-                    q.pop();
-                }
-                break;
-            }
-            while(n--){
-                TreeNode* currnode = q.front();
-                if(currnode->left) q.push(currnode->left);
-                if(currnode->right) q.push(currnode->right);
-                q.pop();
-            }
-            lev++;
+        queue<TreeNode*>temp;
+        temp.push(node);
+        while(!temp.empty()){
+            TreeNode* curr = temp.front();
+            temp.pop();
+            if(!(curr->left && curr->right)) q.push(curr);
+            if(curr->left) temp.push(curr->left);
+            if(curr->right) temp.push(curr->right);
         }
     }
+     
     int insert(int val) {
-        if(check()){
-            TreeNode* temp = node;
-            while(temp->left && temp->right){
-                temp = temp->left;
-            }
-            TreeNode* newnode = new TreeNode(val);
-            int ans = temp->val;
-            temp->left = newnode;
-            return ans;
+        TreeNode* curr = q.front();
+        TreeNode* newnode = new TreeNode(val);
+
+        if(!curr->left ){
+            curr->left = newnode;
         }else{
-            if(depth-2 < 0){
-                if(!node->left){
-                    node->left = new TreeNode(val);
-                }else{
-                    node->right = new TreeNode(val);
-                }
-                return node->val;
-            }
-            else{
-                solve(depth-2);
-                int ans = -1;
-                for (auto it : temp) {
-                    // try under it->left first (level order: left before right)
-                    if (it->left) {
-                        if (!it->left->left) {
-                            it->left->left = new TreeNode(val);
-                            ans = it->left->val;
-                            return ans;
-                        } else if (!it->left->right) {
-                            it->left->right = new TreeNode(val);
-                            ans = it->left->val;
-                            return ans;
-                        }
-                    }
-
-                    // then try under it->right
-                    if (it->right) {
-                        if (!it->right->left) {
-                            it->right->left = new TreeNode(val);
-                            ans = it->right->val;
-                            return ans;
-                        } else if (!it->right->right) {
-                            it->right->right = new TreeNode(val);
-                            ans = it->right->val;
-                            return ans;
-                        }
-                    }
-                }
-
-            }
+            curr->right = newnode;
+            q.pop();
         }
-        return -1;
+        q.push(newnode);
+        return curr->val;
     }
     
     TreeNode* get_root() {
