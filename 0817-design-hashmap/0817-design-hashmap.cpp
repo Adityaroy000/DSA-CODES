35b2e6fec971ff20381af4e3b0c68f1fe90e0dbf
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-08-19 18:48:10 +0530

    Time: 24 ms (62.02%), Space: 71 MB (41.43%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -0,0 +1,73 @@
+class MyHashMap {
+public:
+    int M;
+    vector<vector<pair<int,int>>>arr;
+    MyHashMap() {
+        M = 12000;
+        arr.resize(M,vector<pair<int,int>>{});
+    }
+    
+    int getIndex(int key){
+        return key%M;
+    }
+
+    void put(int key, int val) {
+        int index = getIndex(key);
+        auto &bucket = arr[index];
+
+        auto it = arr[index].end();
+        for(auto iter = bucket.begin();iter!=bucket.end();iter++){
+            if(iter->first == key){
+                it = iter;
+                break;
+            }
+        }
+        if(it == bucket.end()){
+            bucket.push_back({key,val});
+        }else{
+            it->second = val;
+        }
+    }
+    
+    int get(int key) {
+        int index = getIndex(key);
+        auto &bucket = arr[index];
+
+        auto it = arr[index].end();
+        for(auto iter = bucket.begin();iter!=bucket.end();iter++){
+            if(iter->first == key){
+                it = iter;
+                break;
+            }
+        }
+        if(it == bucket.end()){
+            return -1;
+        }else{
+            return it->second;
+        }
+    }
+    
+    void remove(int key) {
+        int index = getIndex(key);
+        auto &bucket = arr[index];
+
+        auto it = arr[index].end();
+        for(auto iter = bucket.begin();iter!=bucket.end();iter++){
+            if(iter->first == key){
+                it = iter;
+                break;
+            }
+        }
+        if(it != bucket.end()){
+            bucket.erase(it);
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
