202681d2552fb3f361ebcb4a3832a4d4db8f78fc
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-08-28 19:51:04 +0530

    Time: 6 ms (70.25%), Space: 55.8 MB (36.79%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,31 +1,34 @@
 class Twitter {
 public:
-    int timestamp;//global timestamp to keep track of order of tweets
-    unordered_map<int,vector<pair<int,int>>>tweets;//userId,{timestamp,tweetId}
-    unordered_map<int,unordered_set<int>>follower;//userId,set of followers
-    Twitter():timestamp(0) {} // for each object twitter timestamp will be created
+    int timestamp;
+    unordered_map<int,vector<pair<int,int>>>tweet;
+    unordered_map<int,unordered_set<int>>following;
+    Twitter() {
+        timestamp = 0;
+    }
     
     void postTweet(int userId, int tweetId) {
-        tweets[userId].push_back({timestamp++,tweetId});
+        tweet[userId].push_back({timestamp++,tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
-        priority_queue<pair<int,int>>feed;//most recent tweets will be at top wrt to timestamp
-        if(tweets.find(userId)!=tweets.end()){//Include the user's own tweets
-            for(const auto& tweet : tweets[userId]){
-                feed.push(tweet);
+        priority_queue<pair<int,int>>feed;
+        if(tweet.find(userId)!=tweet.end()){
+            for(auto & tw : tweet[userId]){
+                feed.push(tw);
             }
         }
-        if(follower.find(userId)!=follower.end()){// Include tweets from users the current user follows
-            for(int followeeId : follower[userId]){
-                if(tweets.find(followeeId)!=tweets.end()){
-                    for(const auto& tweet : tweets[followeeId]){
-                        feed.push(tweet);
+
+        if(following.find(userId)!=following.end()){
+            for(auto &fid : following[userId]){
+                if(tweet.find(fid)!=tweet.end()){
+                    for(auto & tw : tweet[fid]){
+                        feed.push(tw);
                     }
                 }
             }
         }
-        // Extract the top 10 most recent tweets
+
         vector<int>ans;
         int cnt = 0;
         while(!feed.empty() && cnt < 10){
@@ -34,17 +37,18 @@ public:
             cnt++;
         }
         return ans;
+
     }
     
     void follow(int followerId, int followeeId) {
-        if(followerId!=followeeId){// Prevent self-following
-            follower[followerId].insert(followeeId);
+        if(followerId != followeeId){
+            following[followerId].insert(followeeId);
         }
     }
     
     void unfollow(int followerId, int followeeId) {
-        if(follower[followerId].find(followeeId)!= follower[followerId].end()){
-            follower[followerId].erase(followeeId);
+        if(following[followerId].find(followeeId)!=following[followerId].end()){
+            following[followerId].erase(followeeId);
         }
     }
 };
