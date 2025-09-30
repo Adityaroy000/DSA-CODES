cd9b15c752ce611771273da9c787fdc93955b413
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-09-30 07:10:22 +0530

    Time: 0 ms (100%), Space: 27.8 MB (69.13%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
         int n = nums.size();
-        set<pair<int,int>>st;
-        unordered_set<int>el(nums.begin(),nums.end());
-
-        int maxi = *max_element(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            st.insert({nums[i],nums[i]});
-            for(int j=nums[i]+1;j<=2*nums[i];j++){
-                if(j > maxi) break;
-                if(el.count(j)){
-                    st.insert({nums[i],j});
-                }
-            }
-        }
+        sort(nums.begin(),nums.end());
         int maxxor = 0;
-        for(auto &it: st){
-            maxxor = max(maxxor,it.first^it.second);
+        int j = 0;
+        for(int i=0;i<n;i++){
+            j=i;
+           while(j<n && nums[j] <= 2*nums[i]){
+                maxxor = max(maxxor,nums[i]^nums[j]);
+                j++;
+           }
         }
+        
         return maxxor;
     }
 };
