3602d9758065d649f5d19674246764a8cd06def1
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-11-26 21:57:47 +0530

    Time: 32 ms (38.38%), Space: 37.4 MB (5.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,60 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        int n = nums.size();
-        unordered_map<int,int>mpp;
-        for(int i = 0; i<n; i++){
-            if(mpp.find(target-nums[i])!=mpp.end()){
-                ans.push_back(mpp[target-nums[i]]);
-                ans.push_back(i);
-                return ans;
+private:
+    void merge(vector<pair<int,int>>& nums, int left,int mid, int right){
+        int l = left;
+        int r = mid+1;
+        vector<pair<int,int>>temp;
+        while(l<=mid && r<=right){
+            if(nums[l].first<nums[r].first){
+                temp.push_back(nums[l]);
+                l++;
             }else{
-                mpp[nums[i]] = i;
+                temp.push_back(nums[r]);
+                r++;
             }
         }
-        return ans;
+        while(l<=mid){
+            temp.push_back(nums[l]);
+            l++;
+        }
+        while(r<=right){
+            temp.push_back(nums[r]);
+            r++;
+        }
+        for(int i=left;i<=right;i++){
+            nums[i] = temp[i-left];
+        }
+    }
+    void mergeSort(vector<pair<int,int>>& nums,int left,int right){
+        if(left>=right) return;
+        int mid = left+(right-left)/2;
+
+        mergeSort(nums,left,mid);
+        mergeSort(nums,mid+1,right);
+
+        merge(nums,left,mid,right);
+    }
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>>arr;
+        for(int i=0;i<n;i++){
+            arr.push_back({nums[i],i});
+        }
+        mergeSort(arr,0,n-1);
+
+        int left= 0,right = n-1;
+        int sum = 0;
+        while(left<right){
+            sum = arr[left].first + arr[right].first;
+            if(sum > target){
+                right--;
+            }else if(sum < target){
+                left++;
+            }else{
+                return {arr[left].second,arr[right].second};
+            }
+        }
+        return {};
     }
 };
