fd728c1c2d2adcae5f4aa2e239d62c129ecee37d
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-11-29 14:26:02 +0530

    Time: 0 ms (100%), Space: 10.3 MB (3.82%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -12,16 +12,17 @@ public:
             return 1;
         }
         int maxi = 0;
-        for(auto &it:st){
-            char el = it;
-            int cnt = 0;
-            for(int i=0;i<n;i++){
-                if(s[i]==el){
-                    cnt++;
-                }else cnt = 0;
-                maxi = max(maxi,cnt);
-            }
+        
+        char ch = '#';
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]!=ch){
+                ch = s[i];
+                cnt = 1;
+            }else cnt ++;
+            maxi = max(maxi,cnt);
         }
+        
         return maxi;
     }
 };
