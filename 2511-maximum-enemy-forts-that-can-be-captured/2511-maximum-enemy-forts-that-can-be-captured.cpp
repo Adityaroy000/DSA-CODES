0a822f064a90c35e8a71426ab2fdb79dbc1f7489
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-11-29 12:54:00 +0530

    Time: 0 ms (100%), Space: 11.1 MB (43.48%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
@@ -2,26 +2,6 @@ class Solution {
 public:
     int captureForts(vector<int>& forts) {
         int n = forts.size();
-        bool flag = false;
-        for(int i=0;i<n;i++){
-            if(forts[i]== -1) {
-                flag = true;
-                break;
-            }
-        }
-
-        if(!flag) return 0;
-
-        bool flag1 = false;
-        for(int i=0;i<n;i++){
-            if(forts[i]== 1) {
-                flag1 = true;
-                break;
-            }
-        }
-
-        if(!flag1) return 0;
-
         int maxi = 0;
         int previdx = -1;
         for(int i=0;i<n;i++){
