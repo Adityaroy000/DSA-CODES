818496ae56b3b75c437689b254ae3fec60125cf3
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-12-02 18:10:15 +0530

    Time: 4 ms (72.89%), Space: 27.5 MB (43.65%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -6,6 +6,12 @@ public:
         int prev = 0; // to track devices in prev row
         int ans = 0;
 
+        /*
+        Count devices in each row.
+        Keep only rows where count > 0.
+        For each pair of consecutive such rows, add:
+        devices_in_row[i] * devices_in_row[i+1].
+        */
         for(int i=0;i<n;i++){
             int cnt = 0;
             for(int j=0;j<m;j++){
