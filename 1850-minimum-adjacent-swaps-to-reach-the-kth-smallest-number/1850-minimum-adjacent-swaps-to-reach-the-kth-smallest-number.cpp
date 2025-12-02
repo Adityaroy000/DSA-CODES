666a7044073c01b4d0ea6589bdf79481a6a7b3f0
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-12-02 08:21:44 +0530

    Time: 31 ms (19.61%), Space: 9.2 MB (96.73%) - LeetHub

--- 1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
+++ 1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+/*
+Core Idea (Left-to-Right)
+
+You fix the string from left to right.
+
+At each position i, you ask:
+
+“Which digit is supposed to be here in the final string?”
+That digit is t[i].
+
+If num[i] is already t[i], move on.
+
+If not, you pull the correct digit from the right side using adjacent swaps.
+
+To get the needed digit from j to i using only adjacent swaps, you must move it left j - i times. You can’t do better than that.
+
+logic:
+Fix the string from left to right. At each position, pull the correct digit from the right using the smallest number of adjacent swaps (bubble it left). The total number of such steps is the minimum number of adjacent swaps.
+*/
     int getMinSwaps(string num, int k) {
         int n = num.size();
         string t = num;
@@ -25,10 +44,14 @@ public:
         }
         int swaps = 0;
         for(int i=0;i<n;i++){
+            //If in the given string digit already matches the digit in target string, nothing to do
             if(num[i]!=t[i]){
+                // The digit that must be placed here in given string (from the target permutation)
                 char f = t[i];
                 for(int j=i+1;j<n;j++){
+                    // Find the first occurrence of that digit to the RIGHT
                     if(num[j]==f){
+                    // Bubble this digit leftwards until it reaches position i . Each single-step movement is one adjacent swap
                         for(int k=j;k>i;k--){
                             swap(num[k],num[k-1]);
                             swaps++;
