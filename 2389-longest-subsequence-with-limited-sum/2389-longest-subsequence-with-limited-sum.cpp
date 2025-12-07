47ccacb801e739c1f2d2fb1d7bac1f9aa85ff9f6
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-12-07 14:52:50 +0530

    Time: 0 ms (100%), Space: 18 MB (50.34%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -6,16 +6,23 @@ public:
 
         vector<int>ans(m);
         sort(nums.begin(),nums.end());
-
+        vector<int>psum(n);
+        int s = 0;
+        for(int i=0;i<n;i++){
+            s += nums[i];
+            psum[i] = s;
+        }
         for(int i=0;i<m;i++){
-            int sum = 0;
-            int maxlen = 0,len=0;
-            for(int j=0;j<n;j++){
-                sum += nums[j];
-                if(sum<=queries[i]){
-                    len++;
-                    maxlen = max(maxlen,len);
-                }else break;
+            int maxlen = 0;
+            int low=0,high=n-1;
+            while(low<=high){
+                int mid = (high+low)/2;
+                if(psum[mid]<=queries[i]){
+                    maxlen = max(maxlen,mid+1);
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
             ans[i] = maxlen;
         }
