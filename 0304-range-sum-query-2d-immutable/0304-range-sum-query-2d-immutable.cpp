d335a8071eea0497009bea0a977f457a8185f2a0
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2025-12-08 18:13:24 +0530

    Time: 31 ms (32.37%), Space: 149 MB (88.14%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -4,27 +4,33 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-
-        for(int i=0;i<n;i++){
-            vector<int>p;
-            int sum = 0;
-            for(int j=0;j<m;j++){
-                sum += matrix[i][j];
-                p.push_back(sum);
+        psum.resize(n,vector<int>(m));
+        int sum = 0;
+        for(int i=0;i<m;i++){
+            sum += matrix[0][i];
+            psum[0][i] = sum;
+        }
+        for(int i=1;i<n;i++){
+            psum[i][0] = psum[i-1][0]+matrix[i][0];
+        }
+        sum = 0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                psum[i][j] = matrix[i][j]+psum[i][j-1]+psum[i-1][j]-psum[i-1][j-1];
             }
-            psum.push_back(p);
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for(int i=row1;i<=row2;i++){
-            if(col1>0){
-                sum += psum[i][col2]-psum[i][col1-1];
-            }else{
-                sum += psum[i][col2];
-            }
+        if(col1 == 0 && row1 == 0){
+           return psum[row2][col2];
+        }else if(row1 == 0){
+            return psum[row2][col2] - psum[row2][col1-1];
+        }else if(col1==0){
+           return psum[row2][col2]-psum[row1-1][col2];
         }
+
+        int sum = psum[row2][col2]-psum[row2][col1-1]-psum[row1-1][col2]+psum[row1-1][col1-1];
         return sum;
     }
 };
