3d1b394145d037e01785568ac670c6e2b00566ad
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-02 09:18:39 +0530

    Time: 194 ms (25.71%), Space: 153.9 MB (21.14%) - LeetHub

--- 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.cpp
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    /*
+    for given index vector : [p0,p1,p2,........,pk]
+    let say we want find distance of pj from each ele
+    So, left sum = (pj-p0)+(pj-p1)+(pj-p2)+ ..... +(pj-pj-1)
+                 = (pj*j) - (p0+p1+p2+...+pj-1) can be done with prefix sum
+        right sum = (pj+1 -pj)+(pj+2 - pj)+....+(pk-pj)
+                  = (pj+1 + pj+2 + .... + pk)-((k-j)*pj) can also be done with prefix sum
+        answer will be just left sum + right sum.
+    */
     vector<long long> getDistances(vector<int>& arr) {
         int n = arr.size();
         unordered_map<int,vector<int>>mpp;
