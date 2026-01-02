ff7ded5d1f85d022c9db5680ba888d8bc7c0430e
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-02 11:54:37 +0530

    Time: 0 ms (100%), Space: 13.7 MB (83.62%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        int n = event1.size();
-        if(event1[1]>=event2[0] && event2[1]>=event1[0]){
-            return true;
-        }
-        return false;
+        return max(event1[0],event2[0])<=min(event1[1],event2[1]);
     }
 };
