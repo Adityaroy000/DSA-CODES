7b212cd3488098ac20193eedd7ec24004ad974ad
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-03 12:08:51 +0530

    Time: 3 ms (92.58%), Space: 97.8 MB (98.23%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -6,6 +6,27 @@ public:
 
         vector<int>pre(s.size()+1,0);
 
+        /*
+        string is abcda
+        s[0] = a , bit = a-'a' = 0
+        (1<<0) put 1 at 0th position in 26 bit integer => 00000...001
+        s[1] = b, bit = b-'a' = 1
+        (1<<1) put 1 at 1st pos in 26 bit integer => 0000...010
+
+        pre[0] = 0000..000
+        (1<<0) = 0000..001
+               ^ __________
+        pre[1]=  0000..001 (1 means char have odd freq. and 0 means char have even freq)        
+        (1<<1)=  0000..010 
+                ___________
+        pre[2]=  0000..011 (two char have odd freq.)
+
+        A string can be rearranged to form a palindrome if and only if:
+	    At most one character has an odd frequency.
+
+        One replacement can fix two odd-frequency characters.
+        So, oddCount/2 <= k is true.
+        */
         for(int i=0;i<s.size();i++){
             int bit = s[i]-'a';
             pre[i+1] = pre[i] ^ (1 << bit);
