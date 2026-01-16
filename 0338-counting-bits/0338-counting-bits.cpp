096bb8b760ab7af50b8165e4e49dae57cc54a345
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-16 06:38:45 +0530

    Time: 0 ms (100%), Space: 10.6 MB (78.83%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int>ans(n+1,0);
-
+        /*
+            any binary  number is [some bits] + [last bit]
+            so total one = one in [some bits] + one in [last bit]
+            i>>1 -> removes last bit from i
+            i&1 -> tells if last bit is 1 or 0
+        */
         for(int i=0;i<=n;i++){
             ans[i] = ans[i>>1] + (i&1);
         }
