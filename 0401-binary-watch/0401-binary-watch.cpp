4f87f4c83f350541081fca6877b0e71a4b670ff8
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-16 08:12:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (9.07%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    vector<string> readBinaryWatch(int turnedOn) {
-        vector<string>ans;
+    vector<int>hr = {8,4,2,1};
+    vector<int>mn = {32,16,8,4,2,1};
+    vector<string>ans;
+    void solve(int idx, int turnedOn, int h, int m){
+        if(h>11 || m > 59) return;
 
-        for(int h=0;h<12;h++){
-            for(int m=0;m<60;m++){
-                if(__builtin_popcount(h)+__builtin_popcount(m) == turnedOn){
-                    string time = to_string(h)+":";
-                    if(m<10) time += "0";
-                    time += to_string(m);
-                    ans.push_back(time);
-                }
-            }
+        if(turnedOn == 0){
+            string time = to_string(h)+":";
+            if(m<10) time += "0";
+            time += to_string(m);
+            ans.push_back(time);
+            return;
         }
+
+        if(idx == 10) return;
+
+        if(idx<4) solve(idx+1,turnedOn-1,h+hr[idx],m);
+        else solve(idx+1, turnedOn-1,h,m+mn[idx-4]);
+
+        solve(idx+1,turnedOn,h,m);
+    }
+    vector<string> readBinaryWatch(int turnedOn) {
+        solve(0,turnedOn,0,0);
+        
         return ans;
     }
 };
