9d47f4886b942e83f66a5c15dc1b3f05a5b87135
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-22 09:18:51 +0530

    Time: 0 ms (100%), Space: 9.2 MB (76.88%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sumOfS = 0,sumOfT=0;
-        for(int i=0;i<s.size();i++){
-            sumOfS += (int)(s[i]);
-            sumOfT += (int)(t[i]);
-        }
-        sumOfT += t[t.size()-1];
+        char ans = 0;
+        for(int i=0;i<s.size();i++) ans ^= s[i];
+        for(int i=0;i<t.size();i++) ans ^= t[i];
 
-        int dif = sumOfT - sumOfS;
-
-        return (char)dif;
+        return ans;
     }
 };
