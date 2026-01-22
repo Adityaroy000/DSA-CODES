9ffdd834cf6f91e9b95597731ccdffc53b0afcf9
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-01-22 09:14:02 +0530

    Time: 0 ms (100%), Space: 9.3 MB (25.06%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>mpp;
+        int sumOfS = 0,sumOfT=0;
         for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
+            sumOfS += (int)(s[i]);
+            sumOfT += (int)(t[i]);
         }
+        sumOfT += t[t.size()-1];
 
-        char ans;
-        for(int i=0;i<t.size();i++){
-            if(mpp.find(t[i])!=mpp.end()){
-                mpp[t[i]]==1?mpp.erase(t[i]):mpp[t[i]]--;
-            }else{
-                ans = t[i];
-                break;
-            }
-        }
-        return ans;
+        int dif = sumOfT - sumOfS;
+
+        return (char)dif;
     }
 };
