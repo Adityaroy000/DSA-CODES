27fc6019b8a565ea589fdc6712ec705d44ea1d1c
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-02-11 10:21:34 +0530

    Time: 0 ms (100%), Space: 13.9 MB (80.68%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -6,7 +6,17 @@ public:
         for(int num:nums){
             xorAll ^= num;
         }
-
+        /*
+        Find a bit where they differ
+        We need to separate numbers into two groups:
+            group containing a
+            group containing b
+        Use a bit where a and b differ.
+        From xorAll, pick any set bit.
+        Now split numbers into two groups:
+        Group 1 → bit is 1
+        Group 2 → bit is 0
+        */
         long bit = xorAll & (-(long)xorAll);
         int xor1 = 0,xor2=0;
         for(int num:nums){
