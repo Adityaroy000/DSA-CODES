f1ebf85ed50c5f5033bf929309be00bbdee86e85
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-03-04 07:40:19 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.47%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 0, high = INT_MAX;
+
+        while(low<=high){
+            long long mid = low+(high-low)/2;
+
+            if(mid*mid < x){
+                low = mid+1;
+            }else if(mid * mid > x){
+                high = mid-1;
+            }else return mid;
+        }
+
+        return high;
+    }
+};
