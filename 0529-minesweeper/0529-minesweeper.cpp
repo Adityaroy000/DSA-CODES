2039d0fc3ab6a49a2402c1a42c9e9e72b17bec13
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-03-24 08:41:20 +0530

    Time: 1 ms (41.39%), Space: 16.3 MB (35.31%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        int r = board.size();
+        int c = board[0].size();
+        int u = click[0],v=click[1];
+
+        if(board[u][v] == 'M') {
+            board[u][v] = 'X';
+            return board;
+        }
+
+        int drow[] = {-1,-1,-1, 0, 0, 1, 1, 1};
+        int dcol[] = {-1, 0, 1,-1, 1,-1, 0, 1};
+
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(r,vector<int>(c,0));
+
+
+        q.push({u,v});
+        vis[u][v] = 1;
+
+        while(!q.empty()){
+            auto [row, col] = q.front();
+            q.pop();
+
+            int cnt = 0;
+            for(int i=0;i<8;i++){
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+
+                if(nrow>=0 && nrow<r && ncol>=0 && ncol<c && !vis[nrow][ncol]){
+                    if(board[nrow][ncol]=='M') cnt++;
+                }
+            } 
+            if(cnt>0){
+                board[row][col] = cnt+'0';
+            }else{
+                board[row][col] = 'B';
+                for(int i=0;i<8;i++){
+                    int nrow = row+drow[i];
+                    int ncol = col+dcol[i];
+
+                    if(nrow>=0 && nrow<r && ncol>=0 && ncol<c && !vis[nrow][ncol]){
+                       q.push({nrow,ncol});
+                       vis[nrow][ncol] = 1;
+                    }
+                } 
+            }
+        }
+        return board;
+    }
+};
