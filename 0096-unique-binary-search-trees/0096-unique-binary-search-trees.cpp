b841c07995a2055ee4e848155a5073dd1175258a
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-05-28 07:27:06 +0530

    Time: 0 ms (100%), Space: 8 MB (40.21%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-/*
-Catalan(n)= (2n)!/((n+1)!⋅n!)
-*/
     int numTrees(int n) {
-        vector<int> uniqTree(n + 1, 1);
+        // dp[i] = number of unique BSTs that can be formed using i nodes
+        vector<int>dp(n+1,1); // 1 bcz there is always one way to build a bst which is empty 
 
-        for (int nodes = 2; nodes <= n; nodes++) {
+        for(int node=2;node<=n;node++){
             int total = 0;
-            for (int root = 1; root <= nodes; root++) {
-                total += uniqTree[root - 1] * uniqTree[nodes - root];
+            for(int root=1;root<=node;root++){
+                // If 'root' is chosen as the root node:
+                // left subtree will contain (root - 1) nodes
+                // right subtree will contain (node - root) nodes
+
+                // Number of ways to form left subtree  = dp[root - 1]
+                // Number of ways to form right subtree = dp[node - root]
+
+                // Each left subtree can pair with every right subtree
+                // so total combinations for this root are:
+                total += dp[root-1]*dp[node-root];
             }
-            uniqTree[nodes] = total;
+            dp[node] = total;
         }
 
-        return uniqTree[n];  
+        return dp[n];
     }
 };
