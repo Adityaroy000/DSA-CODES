e7e3e081e18d6bee033460808c2f20e919b0039c
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-06-24 07:57:05 +0530

    Time: 0 ms (100%), Space: 9 MB (45.49%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int n = text.size();
+        unordered_map<char,int>mpp;
+
+        for(char ch: text){
+            mpp[ch]++;
+        }
+        int ans = INT_MAX;
+        if(mpp.count('b') && mpp['b'] >= 1){
+            ans = min(ans,mpp['b']);
+        }else ans = 0;
+
+        if(mpp.count('a') && mpp['a'] >= 1){
+            ans = min(ans,mpp['a']);
+        }else ans = 0;
+
+        if(mpp.count('l') && mpp['l'] >= 2){
+            int inst = mpp['l']/2;
+            ans = min(ans,inst);
+        }else ans = 0;
+        if(mpp.count('o') && mpp['o'] >= 2){
+            int inst = mpp['o']/2;
+            ans = min(ans,inst);
+        }else ans = 0;
+        if(mpp.count('n') && mpp['n'] >= 1){
+            ans = min(ans,mpp['n']);
+        }else ans = 0;
+        return ans==INT_MAX?0:ans;
+    }
+};
