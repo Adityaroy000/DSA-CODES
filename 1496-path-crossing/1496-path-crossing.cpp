8c5a65733412ad851ae25c4db73bee74b8d0a690
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-07-06 10:26:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (78.25%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int n = path.size();
+
+        set<pair<int,int>>st;
+
+        st.insert({0,0});
+        int x = 0,y=0;
+        for(int i=0;i<n;i++){
+            if(path[i]=='W') x = x-1;
+            if(path[i]=='S') y = y-1;
+            if(path[i]=='E') x = x+1;
+            if(path[i]=='N') y = y+1;
+
+            if(st.count({x,y})) return true;
+
+            st.insert({x,y});
+        }
+
+        return false;
+    }
+};
