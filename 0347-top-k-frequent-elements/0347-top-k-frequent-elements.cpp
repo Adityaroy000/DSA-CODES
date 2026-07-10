3ce530b5d135725821cd9de2d83a5929ad60f2a9
Aditya roy <159246916+Adityaroy000@users.noreply.github.com>
2026-07-10 10:19:33 +0530

    Time: 1 ms (65.02%), Space: 18 MB (62.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
+    typedef pair<int,int> p;
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int,int>mpp;
-        for(int i = 0; i<n; i++){
-            if(mpp.find(nums[i])!=mpp.end()){
-                mpp[nums[i]]++;
-            }
-            else{
-                mpp[nums[i]] = 1;
-            }
-        }
-        vector<pair<int,int>>temp(mpp.begin(),mpp.end());
-        for(int i = 0; i<temp.size(); i++){
-            for(int j = i+1; j<temp.size(); j++){
-                if(temp[i].second<temp[j].second){
-                    auto it = temp[i];
-                    temp[i] = temp[j];
-                    temp[j] = it;
-                }
-            }
-        }
         vector<int>ans;
-        for(int i = 0;i<k; i++){
-            ans.push_back(temp[i].first);
+        unordered_map<int,int>mpp;
+
+        for(int x : nums) mpp[x]++;
+
+        priority_queue<p,vector<p>>pq;
+
+        for(auto it : mpp){
+            pq.push({it.second,it.first});
         }
+
+        for(int i=1;i<=k;i++){
+            auto [cnt, el] = pq.top();
+            pq.pop();
+            ans.push_back(el);
+        }
+
         return ans;
     }
 };
